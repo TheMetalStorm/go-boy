991ee06674b0052d3b921d3e0cfbbb8d2f391b03
simon <arapoglu.simon@gmx.de>
2025-05-19 05:42:54 +0200

    removed Reg8 enum

--- cpu/reg.go
+++ cpu/reg.go
@@ -1,18 +1,6 @@
 package cpu
 
-type Reg8 int
 type Reg16 int
-
-const (
-	REG_A Reg8 = iota
-	REG_F
-	REG_B
-	REG_C
-	REG_D
-	REG_E
-	REG_H
-	REG_L
-)
 
 const (
 	REG_AF Reg16 = iota
@@ -23,17 +11,6 @@ const (
 	REG_PC
 )
 
-var reg8Name = map[Reg8]string{
-	REG_A: "Register A",
-	REG_F: "Register F",
-	REG_B: "Register B",
-	REG_C: "Register C",
-	REG_D: "Register D",
-	REG_E: "Register E",
-	REG_H: "Register H",
-	REG_L: "Register L",
-}
-
 var reg16Name = map[Reg16]string{
 	REG_AF: "Register AF",
 	REG_BC: "Register BC",
@@ -41,10 +18,6 @@ var reg16Name = map[Reg16]string{
 	REG_HL: "Register HL",
 	REG_SP: "Register SP",
 	REG_PC: "Register PC",
-}
-
-func (ss Reg8) String() string {
-	return reg8Name[ss]
 }
 
 func (ss Reg16) String() string {
