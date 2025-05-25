3658a604e75bb616ee5d38a3a07523e1acb221f9
simon <you@example.com>
2025-05-25 18:15:54 +0200

    code without gio to try and use imgui

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"go-boy/cpu"
 	"go-boy/rom"
 	"go-boy/widgets"
-	"log"
-	"os"
-
-	"gioui.org/app"
-	"gioui.org/layout"
-	"gioui.org/op"
-	"gioui.org/unit"
-	"gioui.org/widget"
-	"gioui.org/widget/material"
+	//"gioui.org/app"
 )
 
 type Cpu = cpu.Cpu
@@ -23,16 +14,16 @@ var c *cpu.Cpu = cpu.NewCpu()
 
 func main() {
 
-	go func() {
-		window := new(app.Window)
-		err := run(window)
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
+	// go func() {
+	// 	window := new(app.Window)
+	// 	err := run(window)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	os.Exit(0)
+	// }()
 	emulate()
-	app.Main()
+	//app.Main()
 
 }
 
@@ -43,7 +34,7 @@ func emulate() {
 
 	c.LoadBootRom(bootrom)
 	// cpu.PatchBootRom(bootrom)
-	c.Autorun = false
+	c.Autorun = true
 
 	for {
 		if c.Autorun {
@@ -58,124 +49,124 @@ func emulate() {
 
 }
 
-func run(window *app.Window) error {
-	window.Option(app.Size(unit.Dp(400), unit.Dp(600)))
+// func run(window *app.Window) error {
+// 	window.Option(app.Size(unit.Dp(400), unit.Dp(600)))
 
-	theme := material.NewTheme()
-	var ops op.Ops
-	var runButton widget.Clickable
-	var stopButton widget.Clickable
-	var stepButton widget.Clickable
-	var regListState widget.List
-	regListState.Axis = layout.Vertical
+// 	theme := material.NewTheme()
+// 	var ops op.Ops
+// 	var runButton widget.Clickable
+// 	var stopButton widget.Clickable
+// 	var stepButton widget.Clickable
+// 	var regListState widget.List
+// 	regListState.Axis = layout.Vertical
 
-	var split Split
-	split.MaxHeight = unit.Dp(200)
-	stepClickedNow := false
+// 	var split Split
+// 	split.MaxHeight = unit.Dp(200)
+// 	stepClickedNow := false
 
-	column := layout.Flex{
-		Axis:    layout.Horizontal,
-		Spacing: layout.SpaceEvenly,
-	}
-	for {
+// 	column := layout.Flex{
+// 		Axis:    layout.Horizontal,
+// 		Spacing: layout.SpaceEvenly,
+// 	}
+// 	for {
 
-		switch e := window.Event().(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
-			// Let's try out the flexbox layout:
-			layout.Flex{
-				// Vertical alignment, from top to bottom
-				Axis: layout.Vertical,
-				// Empty space is left at the start, i.e. at the top
-				Spacing: layout.SpaceStart,
-			}.Layout(gtx,
-				layout.Rigid(
-					func(gtx layout.Context) layout.Dimensions {
-						list := [10]string{}
-						list[0] = fmt.Sprintf("Reg A: 0x%04x", c.A)
-						list[1] = fmt.Sprintf("Reg F: 0x%04x (0b%08b)", c.F, c.F)
-						list[2] = fmt.Sprintf("Reg B: 0x%04x", c.B)
-						list[3] = fmt.Sprintf("Reg C: 0x%04x", c.C)
-						list[4] = fmt.Sprintf("Reg D: 0x%04x", c.D)
-						list[5] = fmt.Sprintf("Reg E: 0x%04x", c.E)
-						list[6] = fmt.Sprintf("Reg H: 0x%04x", c.H)
-						list[7] = fmt.Sprintf("Reg L: 0x%04x", c.L)
-						list[8] = fmt.Sprintf("SP: 0x%04x", c.SP)
-						list[9] = fmt.Sprintf("PC: 0x%04x", c.PC)
+// 		switch e := window.Event().(type) {
+// 		case app.DestroyEvent:
+// 			return e.Err
+// 		case app.FrameEvent:
+// 			gtx := app.NewContext(&ops, e)
+// 			// Let's try out the flexbox layout:
+// 			layout.Flex{
+// 				// Vertical alignment, from top to bottom
+// 				Axis: layout.Vertical,
+// 				// Empty space is left at the start, i.e. at the top
+// 				Spacing: layout.SpaceStart,
+// 			}.Layout(gtx,
+// 				layout.Rigid(
+// 					func(gtx layout.Context) layout.Dimensions {
+// 						list := [10]string{}
+// 						list[0] = fmt.Sprintf("Reg A: 0x%04x", c.A)
+// 						list[1] = fmt.Sprintf("Reg F: 0x%04x (0b%08b)", c.F, c.F)
+// 						list[2] = fmt.Sprintf("Reg B: 0x%04x", c.B)
+// 						list[3] = fmt.Sprintf("Reg C: 0x%04x", c.C)
+// 						list[4] = fmt.Sprintf("Reg D: 0x%04x", c.D)
+// 						list[5] = fmt.Sprintf("Reg E: 0x%04x", c.E)
+// 						list[6] = fmt.Sprintf("Reg H: 0x%04x", c.H)
+// 						list[7] = fmt.Sprintf("Reg L: 0x%04x", c.L)
+// 						list[8] = fmt.Sprintf("SP: 0x%04x", c.SP)
+// 						list[9] = fmt.Sprintf("PC: 0x%04x", c.PC)
 
-						return material.List(theme, &regListState).Layout(gtx, 10, func(gtx layout.Context, index int) layout.Dimensions {
-							return layout.Stack{}.Layout(gtx,
-								layout.Stacked(func(gtx layout.Context) layout.Dimensions {
+// 						return material.List(theme, &regListState).Layout(gtx, 10, func(gtx layout.Context, index int) layout.Dimensions {
+// 							return layout.Stack{}.Layout(gtx,
+// 								layout.Stacked(func(gtx layout.Context) layout.Dimensions {
 
-									return layout.UniformInset(unit.Dp(8)).Layout(gtx, material.Body1(theme, list[index]).Layout)
-								}),
-							)
-						})
-					},
-				),
+// 									return layout.UniformInset(unit.Dp(8)).Layout(gtx, material.Body1(theme, list[index]).Layout)
+// 								}),
+// 							)
+// 						})
+// 					},
+// 				),
 
-				layout.Rigid(
-					func(gtx layout.Context) layout.Dimensions {
+// 				layout.Rigid(
+// 					func(gtx layout.Context) layout.Dimensions {
 
-						return split.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-							a := material.H1(theme, "Hello")
-							return a.Layout(gtx)
-						}, func(gtx layout.Context) layout.Dimensions {
-							a := material.H1(theme, "World")
-							return a.Layout(gtx)
-						})
-					},
-				),
-				layout.Rigid(
-					func(gtx layout.Context) layout.Dimensions {
-						return column.Layout(gtx,
-							layout.Rigid(
-								func(gtx layout.Context) layout.Dimensions {
-									btn := material.Button(theme, &runButton, "Start")
-									return btn.Layout(gtx)
-								},
-							),
-							layout.Rigid(
-								func(gtx layout.Context) layout.Dimensions {
-									btn := material.Button(theme, &stepButton, "Step")
-									return btn.Layout(gtx)
-								},
-							),
-							layout.Rigid(
-								func(gtx layout.Context) layout.Dimensions {
-									btn := material.Button(theme, &stopButton, "Stop")
-									return btn.Layout(gtx)
-								},
-							),
-						)
-					},
-				),
+// 						return split.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+// 							a := material.H1(theme, "Hello")
+// 							return a.Layout(gtx)
+// 						}, func(gtx layout.Context) layout.Dimensions {
+// 							a := material.H1(theme, "World")
+// 							return a.Layout(gtx)
+// 						})
+// 					},
+// 				),
+// 				layout.Rigid(
+// 					func(gtx layout.Context) layout.Dimensions {
+// 						return column.Layout(gtx,
+// 							layout.Rigid(
+// 								func(gtx layout.Context) layout.Dimensions {
+// 									btn := material.Button(theme, &runButton, "Start")
+// 									return btn.Layout(gtx)
+// 								},
+// 							),
+// 							layout.Rigid(
+// 								func(gtx layout.Context) layout.Dimensions {
+// 									btn := material.Button(theme, &stepButton, "Step")
+// 									return btn.Layout(gtx)
+// 								},
+// 							),
+// 							layout.Rigid(
+// 								func(gtx layout.Context) layout.Dimensions {
+// 									btn := material.Button(theme, &stopButton, "Stop")
+// 									return btn.Layout(gtx)
+// 								},
+// 							),
+// 						)
+// 					},
+// 				),
 
-				// ... then one to hold an empty spacer
-				layout.Rigid(
-					// The height of the spacer is 25 Device independent pixels
-					layout.Spacer{Height: unit.Dp(25)}.Layout,
-				),
-			)
+// 				// ... then one to hold an empty spacer
+// 				layout.Rigid(
+// 					// The height of the spacer is 25 Device independent pixels
+// 					layout.Spacer{Height: unit.Dp(25)}.Layout,
+// 				),
+// 			)
 
-			if stepButton.Pressed() && !stepClickedNow {
-				c.DoStep = true
-				stepClickedNow = true
-			} else if !stepButton.Pressed() {
-				stepClickedNow = false
-			}
+// 			if stepButton.Pressed() && !stepClickedNow {
+// 				c.DoStep = true
+// 				stepClickedNow = true
+// 			} else if !stepButton.Pressed() {
+// 				stepClickedNow = false
+// 			}
 
-			if runButton.Pressed() {
-				c.Autorun = true
-			}
-			if stopButton.Pressed() {
-				c.Autorun = false
-			}
-			e.Frame(gtx.Ops)
+// 			if runButton.Pressed() {
+// 				c.Autorun = true
+// 			}
+// 			if stopButton.Pressed() {
+// 				c.Autorun = false
+// 			}
+// 			e.Frame(gtx.Ops)
 
-		}
+// 		}
 
-	}
-}
+// 	}
+// }
